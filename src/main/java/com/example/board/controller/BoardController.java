a34505b4bd9cc0d61844fa79c0f8430b2334aada
antidote <ljh8651@nate.com>
2022-12-09 14:57:03 +0900

    loginplugupdate

---
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -0,0 +1,4 @@
+package com.example.board.entity;
+
+public class Board {
+}
---
+++ src/main/java/com/example/board/dto/BoardResponseDto.java
@@ -0,0 +1,7 @@
+package com.example.board.dto;
+
+public class BoardResponceDto {
+    private String userId;
+    private String userPassword;
+    private String contents;
+}
